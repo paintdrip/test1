db7be9efda8c95a59db30b43b484ef035ec21d16
paintdrippin <pa1nkller8879@gmail.com>
2023-12-24 22:18:20 +0300

    функционал затемнения

--- script.js
+++ script.js
@@ -1,22 +1,54 @@
 console.log('скрипт запущен');
 
-// Получаем кнопки и поле с контентом
-const forwardButton = document.getElementById('forwardButton');
-const backButton = document.getElementById('backButton');
+function updateElementOpacity() {
+  const elements = document.getElementsByClassName('item-block__item');
+
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i];
+    const isVisible = isElementFullyVisible(element);
+
+    if (isVisible) {
+      element.style.opacity = '1';
+    } else {
+      element.style.opacity = '0.2';
+    }
+  }
+}
+
+function isElementFullyVisible(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Вызов функции при загрузке страницы и при изменении размеров окна
+window.addEventListener('load', updateElementOpacity);
+window.addEventListener('resize', updateElementOpacity);
+
+// Слайдер
 const contentField = document.getElementById('contentField');
-
-// Устанавливаем обработчики событий на кнопки
-forwardButton.addEventListener('click', scrollForward);
-backButton.addEventListener('click', scrollBack);
-
+contentField.addEventListener('scroll', updateElementOpacity);
 // Функция для прокрутки вперед
-function scrollForward() {
-  contentField.scrollBy({ left: 250, behavior: 'smooth' }); // Измените значение 100 на нужное количество пикселей для прокрутки
+async function scrollForward() {
+  await contentField.scrollBy({ left: 250, behavior: 'smooth' });
+  updateElementOpacity();
 }
 
 // Функция для прокрутки назад
-function scrollBack() {
-  contentField.scrollBy({ left: -250, behavior: 'smooth' }); // Измените значение 100 на нужное количество пикселей для прокрутки
+async function scrollBack() {
+  await contentField.scrollBy({ left: -250, behavior: 'smooth' });
+  updateElementOpacity();
 }
+
+// Устанавливаем обработчики событий на кнопки
+const forwardButton = document.getElementById('forwardButton');
+forwardButton.addEventListener('click', scrollForward);
+
+const backButton = document.getElementById('backButton');
+backButton.addEventListener('click', scrollBack);
 
 console.log('скрипт закончен');
